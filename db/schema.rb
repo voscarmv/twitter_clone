e03a568999cef3ef7752f6de1f881e936198c12e
Oscar Mier <voscarmv@gmail.com>
2020-04-30 15:42:49 -0500

    Users can now tweeeet

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_174411) do
+ActiveRecord::Schema.define(version: 2020_04_30_203003) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
